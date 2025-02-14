b65e0bf4a659bd80ca576ded0fb304c04bd7d4d9
rajats-dev <rajatsundriyal1@gmail.com>
2025-02-14 13:09:08 +0530

    feat(messages): implement message sending and retrieval functionality

--- apps/server/src/routes/message.routes.ts
+++ apps/server/src/routes/message.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import MessageController from "../controllers/message.controller.js";
 
 const router = Router();
 
-// router.post("/login", login);
-// router.post("/logout", logout);
+router.get(
+  "/conversations",
+  protectRoute,
+  MessageController.getUsersForSidebar
+);
+router.get("/:id", protectRoute, MessageController.getMessage);
+router.post("/send/:id", protectRoute, MessageController.sendMessage);
 
 export default router;
